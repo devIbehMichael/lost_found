e877046903472022ee91b0c768bb76f045045ef0
ibeh michael <ibehmichael8@gmail.com>
2025-09-03 16:36:39 +0100

    I added a push text :)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './output.css'
+
+function App() {
+return (
+    <div className='flex justify-center items-center h-screen'>
+<h2 className="text-3xl font-bold text-blue-500">this is my react app</h2>
+<h2 className="text-3xl font-bold text-blue-500">PUSHED</h2>
+    </div>
+  )
+}
+
+export default App
